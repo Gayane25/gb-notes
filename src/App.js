745fed9dbf05d526916cf79f72053cc5a04231bc
Gayane25 <gayabennu@gmail.com>
2022-07-11 12:42:10 +0400

    feat:added initial configs of firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-
+import {projectFireStore} from "./firebase/firestore"
 import './App.css';
 
 function App() {
+  console.log(projectFireStore)
   return (
     <div>
       barev
---
+++ src/firebase/firestore.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC65uqTfY87qnqGBrcJgYNY65-yjIiofjs",
+    authDomain: "beeweb-task-354af.firebaseapp.com",
+    projectId: "beeweb-task-354af",
+    storageBucket: "beeweb-task-354af.appspot.com",
+    messagingSenderId: "986642471585",
+    appId: "1:986642471585:web:4a84e28c9d1dec5f6aeee0"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  const projectFireStore = firebase.firestore();
+
+  export {projectFireStore}
